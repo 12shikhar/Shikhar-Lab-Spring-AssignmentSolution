cc94334c5e6de83f050c1b9af6ddd2d7f439a8f0
12shikhar <shikharsrivastava748@gmail.com>
2022-07-14 23:13:04 +0530

    Lab6 Solution

---
+++ src/main/java/com/greatlearning/studentmanagement/GlJavafsdLab6Application.java
@@ -0,0 +1,13 @@
+package com.greatlearning.studentmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GlJavafsdLab6Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GlJavafsdLab6Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/greatlearning/StudentManagment/GlJavafsdLab6ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.greatlearning.StudentManagment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GlJavafsdLab6ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
